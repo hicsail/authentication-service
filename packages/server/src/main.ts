22e4a6ee7c03ec15a8f8a1b5c6c4da5e400e7f10
Justin Chen <ch3njus@gmail.com>
2022-11-01 15:01:10 -0400

    Connect prisma client

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 async function bootstrap() {
+  //const user = await prisma.user.create({ data: { name: '' } });
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+bootstrap()
+  .catch((e) => {
+    console.error(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
