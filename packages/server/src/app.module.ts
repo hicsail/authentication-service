7635351e832d6e6c8c89a6f838c5496d3d2d1600
Justin Chen <ch3njus@gmail.com>
2022-11-07 09:20:52 -0500

    chore: removed unused service

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [HealthModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService]
+  providers: []
 })
 export class AppModule {}
