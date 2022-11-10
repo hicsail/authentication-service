a49ab5afbae65fa1f621f206951b3695b08d0383
wenhwang97 <wenhwang@bu.edu>
2022-11-10 16:27:02 -0500

    fix: add return type

--- packages/server/src/user/roles.decorator.ts
+++ packages/server/src/user/roles.decorator.ts
@@ -1,5 +1,5 @@
-import { SetMetadata } from '@nestjs/common';
+import { CustomDecorator, SetMetadata } from '@nestjs/common';
 import { Role } from './enum/role.enum';
 
 export const ROLES_KEY = `roles`;
-export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
+export const Roles = (...roles: Role[]): CustomDecorator<string> => SetMetadata(ROLES_KEY, roles);
