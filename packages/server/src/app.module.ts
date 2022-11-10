b35518ff7c3028614ae39b81b291d6b8028cd4eb
wenhwang97 <wenhwang@bu.edu>
2022-11-10 16:21:34 -0500

    feat: import temp auth module

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -3,9 +3,10 @@ import { HealthModule } from './health/health.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [HealthModule, UserModule],
+  imports: [HealthModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService]
 })
