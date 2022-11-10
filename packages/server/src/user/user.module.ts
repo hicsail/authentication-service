eb85b6cfa019e98e2512ccd4f52ef16bc6a8b9e0
wenhwang97 <wenhwang@bu.edu>
2022-11-10 16:17:46 -0500

    feat: import temp auth module

--- packages/server/src/user/user.module.ts
+++ packages/server/src/user/user.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthModule } from 'src/auth/auth.module';
 import { PrismaService } from '../prisma/prisma.service';
+import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [forwardRef(() => AuthModule), JwtModule.register({ secret: process.env.SECRET })],
+  controllers: [UserController],
   providers: [UserService, PrismaService]
 })
 export class UserModule {}
