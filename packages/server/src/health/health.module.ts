e0f3b7af4457f5cb913c03c080acc92f2af66e24
wenhwang97 <wenhwang@bu.edu>
2022-11-14 14:46:42 -0500

    feat: add database connection health check

--- packages/server/src/health/health.module.ts
+++ packages/server/src/health/health.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { HealthController } from './health.controller';
+import { HealthService } from './health.service';
 
 @Module({
   imports: [],
   controllers: [HealthController],
-  providers: []
+  providers: [HealthService, PrismaService]
 })
 export class HealthModule {}
