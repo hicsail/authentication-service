b54845fad709325b4a54962df85d809df85fee75
wenhwang97 <wenhwang@bu.edu>
2022-11-14 14:54:27 -0500

    style: replace backtick with single quotation

--- packages/server/src/health/health.controller.ts
+++ packages/server/src/health/health.controller.ts
@@ -2,16 +2,16 @@ import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { HealthService } from './health.service';
 
-@Controller(`health`)
+@Controller('health')
 export class HealthController {
   constructor(private readonly healthService: HealthService) {}
 
-  @Get(`live`)
+  @Get('live')
   healthLive(@Res() res: Response): Response<void> {
     return res.status(HttpStatus.NO_CONTENT).send();
   }
 
-  @Get(`ready`)
+  @Get('ready')
   async healthReady(@Res() res: Response): Promise<Response<void>> {
     const dbConnection = this.healthService.isDbReady();
     if (!dbConnection) {
