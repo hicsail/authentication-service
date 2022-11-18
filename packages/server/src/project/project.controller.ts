80c9267736d8ea4b884faa2bbe93ac9d109848cf
Collin Bolles <collinbolles@gmail.com>
2022-11-18 13:45:48 -0500

    feat: Project Module (#11)
    * Create new project
    * Get a list of projects
    * Get detailed project information
    * Update project settings

---
+++ packages/server/src/project/project.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Post, Get, Body, Param, Put } from '@nestjs/common';
+import { Prisma, Project } from '@prisma/client';
+import { ProjectIdentifier, ConfigurableProjectSettings } from './dto/project.dto';
+import { ProjectService } from './project.service';
+
+@Controller('/projects')
+export class ProjectController {
+  constructor(private projectService: ProjectService) {}
+
+  @Post()
+  createProject(@Body() newProject: Prisma.ProjectCreateInput): Promise<Project> {
+    return this.projectService.createProject(newProject);
+  }
+
+  @Get()
+  listProjects(): Promise<ProjectIdentifier[]> {
+    return this.projectService.listProjects();
+  }
+
+  @Get(':id')
+  getProject(@Param('id') id: string): Promise<Project> {
+    return this.projectService.getProject(id);
+  }
+
+  @Put(':id')
+  updateProject(@Param('id') id: string, @Body() settings: ConfigurableProjectSettings): Promise<Project> {
+    return this.projectService.updateProject(id, settings);
+  }
+}
