ea535074cd4db43fa370fe8c95cbfec85d4f7d49
Justin Chen <ch3njus@gmail.com>
2022-11-21 12:28:26 -0500

    fix: regression appeared again

--- packages/server/src/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service'
-import { PrismaService } from '../prisma/prisma.service';
-
-@Module({
-	providers: [UsersService, PrismaService],
-	exports: [UsersService],
-})
-export class UsersModule {}
