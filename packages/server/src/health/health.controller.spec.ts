444d9d75fe5ce49e2a0151b4b57cedc6bffc99b2
Justin Chen <ch3njus@gmail.com>
2022-11-22 13:37:24 -0500

    fix: add HealthService to provider

--- packages/server/src/health/health.controller.spec.ts
+++ packages/server/src/health/health.controller.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
+import { HealthService } from './health.service';
 
 describe('HealthController', () => {
   let healthController: HealthController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [HealthController]
+      controllers: [HealthController],
+      providers: [
+        {
+          provide: HealthService,
+          useValue: jest.fn()
+        }]
     }).compile();
 
     healthController = app.get<HealthController>(HealthController);
