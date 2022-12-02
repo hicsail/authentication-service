2ec4827b5e12fd79c0614fbb6bcc81d74820a78b
Wenhan Wang <wenhwang@bu.edu>
2022-12-01 19:42:23 -0500

    refactor: move duplicated setup to util

--- packages/server/src/user/__tests__/user.controller.spec.ts
+++ packages/server/src/user/__tests__/user.controller.spec.ts
@@ -1,13 +1,13 @@
 import { HttpException } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { Test } from '@nestjs/testing';
 import { Project, User } from '@prisma/client';
 import { PrismaService } from '../../prisma/prisma.service';
 import { UserController } from '../user.controller';
 import { UserService } from '../user.service';
-import * as bcrypt from 'bcrypt';
+import { UserTestUtil } from './utils/user.test.util';
 
 describe('UserModule Integration Test', () => {
+  let userTestUtil: UserTestUtil;
+
   let prisma: PrismaService;
   let userController: UserController;
   let userService: UserService;
@@ -19,60 +19,16 @@ describe('UserModule Integration Test', () => {
   let randomProject: Project;
   let randomUser: User;
 
-  const HASH_ROUNDS = 10;
-
   beforeAll(async () => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [JwtModule.register({ secret: process.env.SECRET })],
-      providers: [PrismaService]
-    }).compile();
+    userTestUtil = new UserTestUtil();
+
+    const moduleRef = await userTestUtil.setup();
 
     prisma = moduleRef.get(PrismaService);
 
-    const dummyProjectsInput = [{ name: 'dummy-project 01' }, { name: 'dummy-project 02' }, { name: 'dummy-project 03' }];
-
-    // adding dummy projects
-    dummyProjects = [];
-    for (const projectInput of dummyProjectsInput) {
-      const project = await prisma.project.create({ data: projectInput });
-      dummyProjects.push(project);
-    }
-    console.log(`${dummyProjects.length} projects successfully created.`);
-
-    const dummyAdminsInput = [
-      { projectId: dummyProjects[0].id, username: 'admin0', email: 'admin0@mail.com', password: await bcrypt.hash('6pD$y38^6HzFcT6P', HASH_ROUNDS), role: 1 },
-      { projectId: dummyProjects[1].id, username: 'admin1', password: await bcrypt.hash('a7kgFU*#26f5KgRP', HASH_ROUNDS), role: 1 },
-      { projectId: dummyProjects[2].id, email: 'admin2@mail.com', password: await bcrypt.hash('kP15!YjF$5cLXUb%', HASH_ROUNDS), role: 1 }
-    ];
-    const dummyUsersInput = [
-      { projectId: dummyProjects[0].id, username: 'proj00-user0', email: 'user0@mail.com', password: await bcrypt.hash('dz$d0I05s4!AmIkN', HASH_ROUNDS) },
-      { projectId: dummyProjects[0].id, username: 'proj00-user1', email: 'user1@mail.com', password: await bcrypt.hash('06!i68UKef87eCUs', HASH_ROUNDS) },
-      { projectId: dummyProjects[0].id, username: 'proj00-user2', email: 'user2@mail.com', password: await bcrypt.hash('V$fd5i9tu@LIHMa8', HASH_ROUNDS) },
-
-      { projectId: dummyProjects[1].id, username: 'proj01-user3', password: await bcrypt.hash('n5I0!2UU6K%y1QEJ', HASH_ROUNDS) },
-      { projectId: dummyProjects[1].id, username: 'proj01-user4', password: await bcrypt.hash('D02WuvmY!BG7Cmj8', HASH_ROUNDS) },
-      { projectId: dummyProjects[1].id, username: 'proj01-user5', password: await bcrypt.hash('Uuq#EoM9*7Q$519^', HASH_ROUNDS) },
-
-      { projectId: dummyProjects[2].id, email: 'user6@mail.com', password: await bcrypt.hash('1X9YrCjya483@bCE', HASH_ROUNDS) },
-      { projectId: dummyProjects[2].id, email: 'user7@mail.com', password: await bcrypt.hash('9wM#HB52MW^98Ni^', HASH_ROUNDS) },
-      { projectId: dummyProjects[2].id, email: 'user8@mail.com', password: await bcrypt.hash('IhS4^F6X6DcfI4#W', HASH_ROUNDS) }
-    ];
-
-    // adding dummy admin users
-    dummyAdmins = [];
-    for (const adminInput of dummyAdminsInput) {
-      const admin = await prisma.user.create({ data: adminInput });
-      dummyAdmins.push(admin);
-    }
-    console.log(`${dummyAdmins.length} admins successfully created.`);
-
-    // adding dummy normal users
-    dummyUsers = [];
-    for (const userInput of dummyUsersInput) {
-      const user = await prisma.user.create({ data: userInput });
-      dummyUsers.push(user);
-    }
-    console.log(`${dummyUsers.length} users successfully created.`);
+    dummyProjects = await userTestUtil.createDummyProjects();
+    dummyAdmins = await userTestUtil.createDummyAdmins();
+    dummyUsers = await userTestUtil.createDummyUsers();
 
     // create controller and service objects
     userService = new UserService(prisma);
@@ -80,13 +36,7 @@ describe('UserModule Integration Test', () => {
   });
 
   afterAll(async () => {
-    const deleteProjects = prisma.project.deleteMany({ where: { id: { in: dummyProjects.map((project) => project.id) } } });
-    const deleteAdmins = prisma.user.deleteMany({ where: { id: { in: dummyAdmins.map((admin) => admin.id) } } });
-    const deleteUsers = prisma.user.deleteMany({ where: { id: { in: dummyUsers.map((user) => user.id) } } });
-
-    await prisma.$transaction([deleteProjects, deleteAdmins, deleteUsers]);
-
-    await prisma.$disconnect();
+    await userTestUtil.tearDown();
   });
 
   beforeEach(async () => {
---
+++ packages/server/src/user/__tests__/utils/user.test.util.ts
@@ -0,0 +1,98 @@
+import { JwtModule } from '@nestjs/jwt';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../../../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { Project, User } from '@prisma/client';
+
+export class UserTestUtil {
+  private prisma: PrismaService;
+
+  private dummyProjects: Project[];
+  private dummyAdmins: User[];
+  private dummyUsers: User[];
+
+  private readonly HASH_ROUNDS = 10;
+
+  async setup(): Promise<TestingModule> {
+    const moduleRef = await Test.createTestingModule({
+      imports: [JwtModule.register({ secret: process.env.SECRET })],
+      providers: [PrismaService]
+    }).compile();
+
+    this.prisma = moduleRef.get(PrismaService);
+
+    return moduleRef;
+  }
+
+  async tearDown(): Promise<void> {
+    const deleteProjects = this.prisma.project.deleteMany({ where: { id: { in: this.dummyProjects.map((project) => project.id) } } });
+    const deleteAdmins = this.prisma.user.deleteMany({ where: { id: { in: this.dummyAdmins.map((admin) => admin.id) } } });
+    const deleteUsers = this.prisma.user.deleteMany({ where: { id: { in: this.dummyUsers.map((user) => user.id) } } });
+
+    await this.prisma.$transaction([deleteProjects, deleteAdmins, deleteUsers]);
+    await this.prisma.$disconnect();
+  }
+
+  async createDummyProjects(): Promise<Project[]> {
+    const dummyProjectsInput = [{ name: 'dummy-project 01' }, { name: 'dummy-project 02' }, { name: 'dummy-project 03' }];
+
+    this.dummyProjects = [];
+    for (const projectInput of dummyProjectsInput) {
+      const project = await this.prisma.project.create({ data: projectInput });
+      this.dummyProjects.push(project);
+    }
+    console.log(`${this.dummyProjects.length} projects successfully created.`);
+
+    return this.dummyProjects;
+  }
+
+  async createDummyAdmins(): Promise<User[]> {
+    if (this.dummyProjects.length <= 0) {
+      return null;
+    }
+
+    const dummyAdminsInput = [
+      { projectId: this.dummyProjects[0].id, username: 'admin0', email: 'admin0@mail.com', password: await bcrypt.hash('6pD$y38^6HzFcT6P', this.HASH_ROUNDS), role: 1 },
+      { projectId: this.dummyProjects[1].id, username: 'admin1', password: await bcrypt.hash('a7kgFU*#26f5KgRP', this.HASH_ROUNDS), role: 1 },
+      { projectId: this.dummyProjects[2].id, email: 'admin2@mail.com', password: await bcrypt.hash('kP15!YjF$5cLXUb%', this.HASH_ROUNDS), role: 1 }
+    ];
+
+    this.dummyAdmins = [];
+    for (const adminInput of dummyAdminsInput) {
+      const admin = await this.prisma.user.create({ data: adminInput });
+      this.dummyAdmins.push(admin);
+    }
+    console.log(`${this.dummyAdmins.length} admins successfully created.`);
+
+    return this.dummyAdmins;
+  }
+
+  async createDummyUsers(): Promise<User[]> {
+    if (this.dummyProjects.length <= 0) {
+      return null;
+    }
+
+    const dummyUsersInput = [
+      { projectId: this.dummyProjects[0].id, username: 'proj00-user0', email: 'user0@mail.com', password: await bcrypt.hash('dz$d0I05s4!AmIkN', this.HASH_ROUNDS) },
+      { projectId: this.dummyProjects[0].id, username: 'proj00-user1', email: 'user1@mail.com', password: await bcrypt.hash('06!i68UKef87eCUs', this.HASH_ROUNDS) },
+      { projectId: this.dummyProjects[0].id, username: 'proj00-user2', email: 'user2@mail.com', password: await bcrypt.hash('V$fd5i9tu@LIHMa8', this.HASH_ROUNDS) },
+
+      { projectId: this.dummyProjects[1].id, username: 'proj01-user3', password: await bcrypt.hash('n5I0!2UU6K%y1QEJ', this.HASH_ROUNDS) },
+      { projectId: this.dummyProjects[1].id, username: 'proj01-user4', password: await bcrypt.hash('D02WuvmY!BG7Cmj8', this.HASH_ROUNDS) },
+      { projectId: this.dummyProjects[1].id, username: 'proj01-user5', password: await bcrypt.hash('Uuq#EoM9*7Q$519^', this.HASH_ROUNDS) },
+
+      { projectId: this.dummyProjects[2].id, email: 'user6@mail.com', password: await bcrypt.hash('1X9YrCjya483@bCE', this.HASH_ROUNDS) },
+      { projectId: this.dummyProjects[2].id, email: 'user7@mail.com', password: await bcrypt.hash('9wM#HB52MW^98Ni^', this.HASH_ROUNDS) },
+      { projectId: this.dummyProjects[2].id, email: 'user8@mail.com', password: await bcrypt.hash('IhS4^F6X6DcfI4#W', this.HASH_ROUNDS) }
+    ];
+
+    this.dummyUsers = [];
+    for (const userInput of dummyUsersInput) {
+      const user = await this.prisma.user.create({ data: userInput });
+      this.dummyUsers.push(user);
+    }
+    console.log(`${this.dummyUsers.length} users successfully created.`);
+
+    return this.dummyUsers;
+  }
+}
