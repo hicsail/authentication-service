bd46c1bf7bfe56b0e614763ab4b6b406c2f8bbe5
Wenhan Wang <wenhwang@bu.edu>
2022-12-06 16:52:46 -0500

    fix: fix lint

--- packages/server/src/project/project.guard.ts
+++ packages/server/src/project/project.guard.ts
@@ -1,9 +1,13 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import { ProjectService } from './project.service';
 
 @Injectable()
 export class ProjectGuard implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
+  constructor(private readonly projectService: ProjectService) {}
+
+  use(req: Request, res: Response, next: NextFunction): void {
     // TODO: All traffic to routes other than `/projects` will check project ID validity
+    next();
   }
 }
