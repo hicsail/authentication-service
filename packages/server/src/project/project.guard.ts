0a20c53468f5e20e91ffc70ae92bfb018a2695f9
Wenhan Wang <wenhwang@bu.edu>
2022-12-07 10:20:09 -0500

    feat: end req-res cycle if project not exists

--- packages/server/src/project/project.guard.ts
+++ packages/server/src/project/project.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import { ProjectService } from './project.service';
 
@@ -8,6 +8,11 @@ export class ProjectGuard implements NestMiddleware {
 
   use(req: Request, res: Response, next: NextFunction): void {
     // TODO: All traffic to routes other than `/projects` will check project ID validity
+    if (!this.projectService.exists(req.body.projectId)) {
+      res.status(HttpStatus.BAD_REQUEST).send('Project ID does not exist');
+      return;
+    }
+
     next();
   }
 }
