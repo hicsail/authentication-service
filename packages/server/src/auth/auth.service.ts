237f970f6bafc1c847eeef406562035b4fcd0e9f
Justin Chen <ch3njus@gmail.com>
2022-12-14 11:41:53 -0500

    chore: remove console.log

--- packages/server/src/auth/auth.service.ts
+++ packages/server/src/auth/auth.service.ts
@@ -45,8 +45,6 @@ export class AuthService {
     if (projectId != null && email != null && password != null) {
       const user = await this.userService.findUserByEmail(projectId, email);
 
-      console.log(user);
-
       if (user && (await bcrypt.compare(password, user.password))) {
         const payload = { id: user.id, projectId: user.projectId, role: user.role };
         return { accessToken: this.jwtService.sign(payload, { expiresIn: process.env.JWT_EXPIRATION }) };
