ecef5326a17085a9448a746fe9c285f567af8454
cbolles <collinbolles@gmail.com>
2022-12-16 13:36:16 -0500

    Add query for getting all projects

--- packages/server/src/project/project.resolver.ts
+++ packages/server/src/project/project.resolver.ts
@@ -1,4 +1,4 @@
-import { Mutation, Args, Resolver } from '@nestjs/graphql';
+import { Mutation, Args, Resolver, Query } from '@nestjs/graphql';
 import { ProjectCreateInput } from './dto/project.dto';
 import { ProjectService } from './project.service';
 import { ProjectModel } from './model/project.model';
@@ -10,5 +10,10 @@ export class ProjectResolver {
   @Mutation(() => ProjectModel)
   async createProject(@Args('project') project: ProjectCreateInput): Promise<ProjectModel> {
     return this.projectService.createProject(project);
+  }
+
+  @Query(() => [ProjectModel])
+  async listProjects(): Promise<ProjectModel[]> {
+    return this.projectService.getAllProjects();
   }
 }
