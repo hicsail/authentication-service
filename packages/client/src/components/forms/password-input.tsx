9c7fccc3d98f8c8416486a2e773d25f73de77fda
Greg Frasco <frascog17@gmail.com>
2022-12-19 17:41:15 -0500

    fix: hide password on submit

--- packages/client/src/components/forms/password-input.tsx
+++ packages/client/src/components/forms/password-input.tsx
@@ -1,21 +1,29 @@
 import { FC, useState } from 'react';
 import { IconButton, InputAdornment } from '@mui/material';
+import { useFormikContext } from 'formik';
 import { TextInput, TextInputProps } from './text-input';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 export const PasswordInput: FC<TextInputProps> = (props) => {
   const [showPassword, setShowPassword] = useState(false);
+  const { isSubmitting } = useFormikContext<any>();
 
   return (
     <TextInput
       {...props}
-      type={showPassword ? 'text' : 'password'}
+      type={!isSubmitting && showPassword ? 'text' : 'password'}
       InputProps={{
         endAdornment: (
           <InputAdornment position="end">
-            <IconButton aria-label="toggle password visibility" onClick={() => setShowPassword(!showPassword)} onMouseDown={(event) => event.preventDefault()} edge="end">
-              {!!showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+            <IconButton
+              disabled={isSubmitting}
+              aria-label="toggle password visibility"
+              onClick={() => setShowPassword(!showPassword)}
+              onMouseDown={(event) => event.preventDefault()}
+              edge="end"
+            >
+              {!isSubmitting && showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
             </IconButton>
           </InputAdornment>
         )
