82e7a403215b40b7001e4fe4f667e63e56fe48d1
Greg Frasco <frascog17@gmail.com>
2022-12-20 11:10:16 -0500

    feat: add login mutation

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,17 +1,23 @@
 import { FC } from 'react';
-import { SettingsProvider } from './context/settings';
+import { SettingsProvider } from './context/settings.context';
 import { ThemeProvider } from './theme/theme.provider';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { SignIn } from './pages/sign-in';
+import { GraphqlProvider } from './graphql/graphql-provider';
+import { ProjectProvider } from './context/project.context';
 
 export const App: FC = () => (
-  <SettingsProvider>
-    <ThemeProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
-  </SettingsProvider>
+  <GraphqlProvider>
+    <SettingsProvider>
+      <ThemeProvider>
+        <Router>
+          <ProjectProvider>
+            <Routes>
+              <Route path="/" element={<SignIn />} />
+            </Routes>
+          </ProjectProvider>
+        </Router>
+      </ThemeProvider>
+    </SettingsProvider>
+  </GraphqlProvider>
 );
---
+++ packages/client/src/context/project.context.tsx
@@ -0,0 +1,56 @@
+import { Backdrop, CircularProgress, ThemeProvider, createTheme, useTheme } from '@mui/material';
+import React, { createContext, FC, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { ProjectModel } from '../graphql/graphql';
+import { useGetProjectQuery } from '../graphql/project/project';
+
+export interface ProjectContextProps {
+  project?: ProjectModel;
+}
+
+const ProjectContext = createContext<ProjectContextProps>({} as ProjectContextProps);
+
+export interface ProjectProviderProps {
+  children: React.ReactNode;
+}
+
+export const ProjectProvider: FC<ProjectProviderProps> = ({ children, ...props }) => {
+  const [project, setProject] = useState<ProjectModel>();
+  const { search } = useLocation();
+  const theme = useTheme();
+  const id = new URLSearchParams(search).get('projectId') || '';
+  const redirectUrl = new URLSearchParams(search).get('redirectUrl') || '';
+  const { data } = useGetProjectQuery({
+    variables: {
+      id
+    },
+    skip: !id
+  });
+
+  useEffect(() => {
+    if (data && data.getProject) {
+      setProject({
+        ...data.getProject,
+        redirectUrl: redirectUrl || data.getProject.redirectUrl
+      } as ProjectModel);
+    }
+  }, [data]);
+
+  return (
+    <ProjectContext.Provider value={{ project }} {...props}>
+      <Backdrop open={!project} sx={{ backgroundColor: (theme) => theme.palette.background.default, zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <CircularProgress color="primary" />
+      </Backdrop>
+      <ThemeProvider
+        theme={createTheme({
+          ...theme,
+          ...project?.muiTheme
+        })}
+      >
+        {children}
+      </ThemeProvider>
+    </ProjectContext.Provider>
+  );
+};
+
+export const useProject = () => React.useContext(ProjectContext);
