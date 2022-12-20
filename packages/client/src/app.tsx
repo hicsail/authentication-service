205b1c224266e722617d1435dbc9fa2efbaa70e4
Greg Frasco <frascog17@gmail.com>
2022-12-20 18:13:52 -0500

    fix: change to browser router

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { SettingsProvider } from './context/settings.context';
 import { ThemeProvider } from './theme/theme.provider';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SignIn } from './pages/sign-in';
 import { GraphqlProvider } from './graphql/graphql-provider';
 import { ProjectProvider } from './context/project.context';
