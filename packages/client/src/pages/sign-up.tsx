4600e7312a2c0367e539a4741541b33fb9806a88
Akosah <emmanuel.s.akosah.19@gmail.com>
2023-01-20 14:49:18 -0500

    modified existing use check, made username optional on client

--- packages/client/src/pages/sign-up.tsx
+++ packages/client/src/pages/sign-up.tsx
@@ -71,15 +71,19 @@ export const SignUp = () => {
           validateOnBlur={false}
           validateOnChange={false}
           validationSchema={SignUpValidation}
-          initialValues={{ email: '', username: '', password: '' }}
+          initialValues={{ email: '', username: undefined, password: '' }}
           onSubmit={async ({ email, username, password }) => {
             setErrorText('');
-            await signUpEmail({ variables: { email, username, password, projectId: project?.id || '' } });
+            // var signUpDetails = { email, password, projectId: project?.id || ''}
+            // if(username){
+            //     signUpDetails = {... signUpDetails ,username}
+            // }
+            await signUpEmail({ variables: { email, password, username, projectId: project?.id || '' } });
           }}
         >
           <Form>
             <TextInput autoFocus fullWidth name="email" label="Email Address" type="email" autoComplete="email" margin="normal" required />
-            <TextInput autoFocus fullWidth name="username" label="username" margin="normal" required />
+            <TextInput autoFocus fullWidth name="username" label="username" margin="normal" />
 
             <PasswordInput name="password" label="Password" fullWidth autoComplete="current-password" required margin="normal" />
             <SubmitButton fullWidth variant="contained" color="primary" sx={{ my: 2 }}>
