771c966fd74334b086eb9d88db14b4434e0f7041
Greg Frasco <frascog17@gmail.com>
2023-04-05 14:52:14 -0400

    fix: add env var into scope

--- packages/client/src/graphql/graphql-provider.tsx
+++ packages/client/src/graphql/graphql-provider.tsx
@@ -2,8 +2,6 @@ import React, { FC } from 'react';
 import { ApolloClient, ApolloProvider, from, HttpLink, InMemoryCache } from '@apollo/client';
 import { env } from '@utils/env';
 
-const uri = `${env.VITE_AUTH_SERVICE}/graphql`;
-
 export interface GraphqlProviderProps {
   children: React.ReactNode;
 }
@@ -11,7 +9,7 @@ export interface GraphqlProviderProps {
 export const GraphqlProvider: FC<GraphqlProviderProps> = ({ children }) => {
   const httpLink = new HttpLink({
     fetch: fetch,
-    uri
+    uri: `${env.VITE_AUTH_SERVICE}/graphql`
   });
   const apolloClient = new ApolloClient({
     cache: new InMemoryCache({
