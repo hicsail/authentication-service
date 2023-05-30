179e198532f2baa31a551fd9bfebcb42fa0cea61
Shashank Karthikeyan <karthish@bu.edu>
2023-05-30 14:52:22 -0400

    Wrapped form and buttons in Fragment, Replaced Grid with box for Google button, More descriptive error message

--- packages/client/src/pages/sign-in.tsx
+++ packages/client/src/pages/sign-in.tsx
@@ -13,7 +13,6 @@ import { Paths } from '@constants/paths';
 import { ProjectDisplay } from '@components/project-display';
 import { useSnackbar } from '@context/snackbar.context';
 import { GoogleLogin } from '@react-oauth/google';
-import { useGoogleLogin } from '@react-oauth/google';
 
 const LoginValidation = Yup.object().shape({
   email: Yup.string().email('Invalid email').required('Required'),
@@ -61,76 +60,69 @@ export const SignIn = () => {
         }}
       >
         <ProjectDisplay project={project} />
-        {project?.authMethods.emailAuth ? (
-          <Formik
-            validateOnBlur={false}
-            validateOnChange={false}
-            validationSchema={LoginValidation}
-            initialValues={{ email: '', password: '' }}
-            onSubmit={async ({ email, password }) => {
-              await loginEmail({ variables: { email, password, projectId: project?.id || '' } });
-            }}
-          >
-            <Form>
-              <Card>
-                <CardHeader title="Sign in" />
-                <CardContent>
-                  <TextInput autoFocus fullWidth name="email" label="Email Address" type="email" autoComplete="email" margin="normal" required />
-                  <PasswordInput name="password" label="Password" fullWidth autoComplete="current-password" required margin="normal" />
-                </CardContent>
-              </Card>
-              <SubmitButton fullWidth variant="contained" color="primary" sx={{ my: 2 }}>
-                Sign In
-              </SubmitButton>
-            </Form>
-          </Formik>
-        ) : (
-          <></>
+        {project?.authMethods.emailAuth && (
+          <>
+            <Formik
+              validateOnBlur={false}
+              validateOnChange={false}
+              validationSchema={LoginValidation}
+              initialValues={{ email: '', password: '' }}
+              onSubmit={async ({ email, password }) => {
+                await loginEmail({ variables: { email, password, projectId: project?.id || '' } });
+              }}
+            >
+              <Form>
+                <Card>
+                  <CardHeader title="Sign in" />
+                  <CardContent>
+                    <TextInput autoFocus fullWidth name="email" label="Email Address" type="email" autoComplete="email" margin="normal" required />
+                    <PasswordInput name="password" label="Password" fullWidth autoComplete="current-password" required margin="normal" />
+                  </CardContent>
+                </Card>
+                <SubmitButton fullWidth variant="contained" color="primary" sx={{ my: 2 }}>
+                  Sign In
+                </SubmitButton>
+              </Form>
+            </Formik>
+          
+            <Grid container>
+              {project?.authMethods.emailAuth && (
+                <Grid item xs>
+                  <Button
+                    onClick={() => {
+                      navigate(Paths.FORGOT_PASSWORD);
+                    }}
+                  >
+                    <Typography variant="body2">Forgot password?</Typography>
+                  </Button>
+                </Grid>
+              )}
+              {project?.settings.allowSignup && (
+                <Grid item>
+                  <Button
+                    onClick={() => {
+                      navigate(Paths.SIGN_UP);
+                    }}
+                  >
+                    <Typography variant="body2">Don't have an account? Sign up</Typography>
+                  </Button>
+                </Grid>
+              )}
+            </Grid>
+          </>
         )}
-        <Grid container>
-          {project?.authMethods.emailAuth ? (
-            <Grid item xs>
-              <Button
-                onClick={() => {
-                  navigate(Paths.FORGOT_PASSWORD);
-                }}
-              >
-                <Typography variant="body2">Forgot password?</Typography>
-              </Button>
-            </Grid>
-          ) : (
-            <></>
-          )}
-          {project?.settings.allowSignup ? (
-            <Grid item>
-              <Button
-                onClick={() => {
-                  navigate(Paths.SIGN_UP);
-                }}
-              >
-                <Typography variant="body2">Don't have an account? Sign up</Typography>
-              </Button>
-            </Grid>
-          ) : (
-            <></>
-          )}
-        </Grid>
-        <Grid container>
-          {project?.authMethods.googleAuth ? (
-            <Grid item>
-              <GoogleLogin
-                onSuccess={({ credential }) => {
-                  loginGoogle({ variables: { projectId: project?.id || '', credential: credential as string } }).catch((err) => {
-                    console.log(err);
-                    pushMessage('Invalid Credentials');
-                  });
-                }}
-              />
-            </Grid>
-          ) : (
-            <></>
-          )}
-        </Grid>
+        {project?.authMethods.googleAuth && (
+          <Box>
+            <GoogleLogin
+              onSuccess={({ credential }) => {
+                loginGoogle({ variables: { projectId: project?.id || '', credential: credential as string } }).catch((err) => {
+                  console.log(err);
+                  pushMessage('Unable to Authenticate with Google');
+                });
+              }}
+            />
+          </Box>
+        )}
       </Box>
     </Container>
   );
