194ec95e4dcdfcadf7bc322838e3027d9e9d00e7
Harunobu Ishii <harunobuishii547@gmail.com>
2023-06-02 13:25:29 -0400

    Minor change

--- packages/server/src/auth/auth.resolver.ts
+++ packages/server/src/auth/auth.resolver.ts
@@ -47,10 +47,10 @@ export class AuthResolver {
   @Mutation(() => Boolean)
   @UsePipes(new ForgotPasswordTransformPipe())
   async forgotPassword(@Args('user') user: ForgotDto): Promise<boolean> {
+    // GraphQL needs something to return
     await this.authService.forgotPassword(user.projectId, user.email);
     this.logger.log('Forgot PW Clicked');
 
-    // GraphQL needs something to return
     return true;
   }
 
@@ -58,10 +58,10 @@ export class AuthResolver {
   @Mutation(() => Boolean)
   @UsePipes(new ResetPasswordTransformPipe())
   async resetPassword(@Args('user') user: ResetDto): Promise<boolean> {
+    // GraphQL needs something to return
     this.logger.log('PW Reset Successful');
     this.authService.resetPassword(user.projectId, user.email, user.password, user.code);
     return true;
-    // GraphQL needs something to return
   }
 
   /** Return Public Key */
